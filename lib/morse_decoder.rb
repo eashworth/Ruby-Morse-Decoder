5c0a9e9f32d5d3fb5cf447ba6f07bbc103284124
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2020-01-26 19:20:12 +0000

    Pass test: Morse_decoder responds to decode_morse_character method with 1 arg

--- lib/morse_decoder.rb
+++ lib/morse_decoder.rb
@@ -1,2 +1,4 @@
 class Morse_decoder
+  def decode_morse_character(character)
+  end
 end
--- spec/morse_decoder_spec.rb
+++ spec/morse_decoder_spec.rb
@@ -1,5 +1,7 @@
 require 'morse_decoder.rb'
 
 describe Morse_decoder do
-  
+  it 'converts a single Morse character to English' do
+    expect(subject).to respond_to(:decode_morse_character).with(1).argument
+  end
 end
